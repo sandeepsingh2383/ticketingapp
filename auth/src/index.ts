883ba40db866f2af3de0a5d3b734f175b5e54eb9
sandeepsingh2383@gmail.com <sandeepsingh2383@gmail.com>
2023-10-30 17:31:53 +0530

    Implement password hashing and pre save hook

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,41 @@
 import express from 'express';
 import 'express-async-errors';
+import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import {json} from 'body-parser';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+
 const app = express();
 app.use(json());
+
 app.use(currentUserRouter);
 app.use(signinRouter);
-app.use(signupRouter);
 app.use(signoutRouter);
-app.all('*', async()=>{
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
   throw new NotFoundError();
-})
+});
+
 app.use(errorHandler);
 
-const start = async() =>{
-  try{
+const start = async () => {
+  try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to mongo db')
-  } catch(err) {
-    console.log(err);
+
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
   }
-}
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+
 start();
-app.listen(3000, () => {
-  console.log('listening on port 3000');
-})
